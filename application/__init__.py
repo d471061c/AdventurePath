3cd27fa19390a510dd185559d43261203e355449
d471061c <cpastethat@gmail.com>
2018-11-05 17:20:06 +0200

    Refactor application folder

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,4 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
-def homepage():
-    return "Hello, AdventurePath."
-
-if __name__ == '__main__':
-    app.run(debug = True)
+from application import views
